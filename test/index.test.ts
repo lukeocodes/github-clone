f46351b195ad8dbb9224e638f0a6f9fcc12f0eb9
Luke Oliff <luke@lukeoliff.com>
2023-07-30 11:51:19 +0100

    chore: passing tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -4,6 +4,10 @@ import { expect } from "chai";
 describe("testing the clone repo functionality", () => {
   it("it should import the cloneRepo package", () => {
     expect(cloneRepo).is.not.undefined;
-    expect(cloneRepo({ repo: "lukeocodes/github-clone" })).is.empty;
+  });
+
+  it("it should not throw an error when cloning a repo", () => {
+    expect(cloneRepo).is.not.undefined;
+    expect(cloneRepo({ repo: "lukeocodes/github-clone" })).to.not.throw;
   });
 });
