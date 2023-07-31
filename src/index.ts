e26011c0059c6b6b0251cfc49ebe02e43abe5346
Luke Oliff <luke@lukeoliff.com>
2023-07-31 13:05:27 +0100

    feat: ping url early in the process before trying to fork or clone

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,46 @@
 // import gitclone from "git-clone";
 import parse from "git-url-parse";
 import { CloneRepoOptions } from "./types/CloneRepoOptions";
+import { request } from "https";
 // import rimraf from "rimraf";
 
-const cloneRepo = ({ repo, dest = "./", ...opts }: CloneRepoOptions): void => {
+const pingUrl = async (ping: string): Promise<boolean> => {
+  const url = new URL(ping);
+
+  const { host, pathname } = url;
+  const opt = { method: "HEAD", host, path: pathname, timeout: 2000 };
+
+  return new Promise((resolve) => {
+    const req = request(opt, (r) => resolve(/4\d\d/.test(`${r.statusCode}`) === false));
+
+    req.on("timeout", () => {
+      req.destroy();
+    });
+
+    req.on("error", () => resolve(false));
+    req.end();
+  });
+};
+
+const cloneRepo = async ({ repo, dest = "./", ...opts }: CloneRepoOptions): Promise<void> => {
   let url;
+
+  if (!/(github\.com)/.test(repo) && !/^([\w-\.]+)\/([\w-\.]+)$/.test(repo)) {
+    throw new Error("That doesn't appear to be a valid GitHub address or org/repo name");
+  }
 
   try {
     const { full_name } = parse(repo);
     url = `https://github.com/${full_name}`;
+
+    if (!(await pingUrl(url))) {
+      throw new Error("The repo does not exist as a public repository on github.com");
+    }
   } catch (e) {
-    throw new Error("Could not parse string into github.com URL");
+    if (e instanceof Error)
+      throw new Error("Could not parse string into a github.com URL" + e.stack);
+
+    throw e;
   }
 
   const { fork } = opts;
