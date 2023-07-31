00307a0f310b3a464e8717e8352238b87e5ac123
Luke Oliff <luke@lukeoliff.com>
2023-07-31 09:15:03 +0100

    feat: parsing repo name into GitHub url

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
 // import gitclone from "git-clone";
+import parse from "git-url-parse";
 import { CloneRepoOptions } from "./types/CloneRepoOptions";
 // import rimraf from "rimraf";
 
 const cloneRepo = ({ repo, dest = "./", ...opts }: CloneRepoOptions): void => {
-  console.log(repo, dest, opts);
+  let url;
+
+  try {
+    const { full_name } = parse(repo);
+    url = `https://github.com/${full_name}`;
+  } catch (e) {
+    throw new Error("Could not parse string into github.com URL");
+  }
+
+  const { fork } = opts;
+
+  if (fork && !process.env.GITHUB_TOKEN) {
+    throw new Error("To fork a repo, `GITHUB_TOKEN` must be set in your environment variables");
+  }
+
+  console.log(url);
+
   return;
 };
 
--- test/index.test.ts
+++ test/index.test.ts
@@ -6,8 +6,21 @@ describe("testing the clone repo functionality", () => {
     expect(cloneRepo).is.not.undefined;
   });
 
-  it("it should not throw an error when cloning a repo", () => {
-    expect(cloneRepo).is.not.undefined;
-    expect(cloneRepo({ repo: "lukeocodes/github-clone" })).to.not.throw;
+  it("it should not throw an error when cloning a repo from org/repo string", () => {
+    expect(() => cloneRepo({ repo: "lukeocodes/github-clone" })).to.not.throw;
+  });
+
+  it("it should not throw an error when cloning a repo from github.com url", () => {
+    expect(() => cloneRepo({ repo: "https://github.com/lukeocodes/github-clone" })).to.not.throw;
+  });
+
+  it("it should not throw an error when cloning a repo from github.com ssh address", () => {
+    expect(() => cloneRepo({ repo: "git@github.com:lukeocodes/github-clone.git" })).to.not.throw;
+  });
+
+  it("it should error when trying to clone a non-repo string", () => {
+    expect(() => cloneRepo({ repo: "werwgwergwg" })).to.throw(
+      "Could not parse string into github.com URL"
+    );
   });
 });
